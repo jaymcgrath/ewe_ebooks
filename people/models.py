7e4b5596343b678b9ea2666f4a1632b39ad024d3
Jay McGrath <jaymcgrath@gmail.com>
2016-11-29 16:36:56 -0800

    added Mashup, MashupAlgorithm, and Output models

--- people/models.py
+++ people/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from contrib.auth import User
 
 # Create your models here.
+
+class Member(models.Model):
+    """
+    one to one related to contrib.auth.User
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    offline_name = models.CharField(max_length=88, null=True, blank=True)
+    email = models.EmailField()
+    bio = models.TextField(null=True, blank=True)
+    dob = models.DateTimeField(auto_now=False)
+    last_active = models.DateTimeField(auto_now=True)
+
