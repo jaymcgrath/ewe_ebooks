f3894b12776707ede9e7cceb3c0e949497149ffc
Jay McGrath <jaymcgrath@gmail.com>
2016-11-30 17:16:48 -0800

    add test for MashupAlgorithm

--- content/tests.py
+++ content/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
 
 # Create your tests here.
+class TestMashupAlgorithm(TestCase):
+    def setUp(self):
+        self.random_instance = mixer.blend('content.MashupAlgorithm', name='mouse_join')
+
+    def tearDown(self):
+        pass
+
+    def test_mashup_algorithm_name(self):
+        self.assertEquals(random_instance.name, 'mouse_join')
