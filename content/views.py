cc5feceeb2081075604d069bfad57dbf08aea128
Jay McGrath <jaymcgrath@gmail.com>
2016-12-02 14:43:29 -0800

    fixes mashup database insertion

--- content/views.py
+++ content/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-
+from django.views.generic import ListView
 from .forms import MashupForm
 
 
@@ -9,4 +9,7 @@ class CreateMashupView(CreateView):
     template_name = 'create_mashup.html'
     form_class = MashupForm
     success_url = '/view_mashups/'
+
+class ListMashupsView(ListView):
+    pass
 
