021e2cb5234ba2359c6d121b1770df813b4fd512
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-01-06 18:55:45 -0800

    buildout two-way form for adding twitter or excerpt based copus

--- sources/forms.py
+++ sources/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Corpus, Word, Bigram, Trigram, Quadgram, Sentence, Hashtag
+from .models import Corpus, Word, Bigram, Trigram, Quadgram, Sentence
 
 
 class CorpusForm(forms.ModelForm):
@@ -8,7 +8,48 @@ class CorpusForm(forms.ModelForm):
     """
     class Meta:
         model = Corpus
-        fields = ('twitter_username',)
+        fields = ('twitter_username', 'variety', 'title', 'body', 'description',)
+
+    def __init__(self, *args, **kwargs):
+        super(CorpusForm, self).__init__(*args, **kwargs)
+
+        # Set some additional field attributes
+
+        variety_attrs = {
+                'class': 'form-control',
+                'placeholder': 'Source Type',
+                'aria-label': 'Input the source type',
+                'id': 'variety'
+                }
+        self.fields['variety'].widget.attrs.update(variety_attrs)
+
+        desc_attrs = {
+                'class':'form-control',
+                'placeholder': 'A brief description of this source',
+                'aria-label': 'Input for source description'
+                }
+        self.fields['description'].widget.attrs.update(desc_attrs)
+
+        title_attrs = {
+                'class':'form-control',
+                'placeholder': 'Choose a title for this source',
+                'aria-label': 'Input for source title'
+        }
+        self.fields['title'].widget.attrs.update(title_attrs)
+
+        body_attrs = {
+            'class': 'form-control',
+            'placeholder': 'Paste the body of the excerpt here',
+            'aria-label': 'Input for source body'
+        }
+        self.fields['body'].widget.attrs.update(body_attrs)
+
+        twitter_username_attrs = {
+            'type':'text',
+            'placeholder':"username",
+            'aria-describedby':"username1"
+        }
+        self.fields['twitter_username'].widget.attrs.update(twitter_username_attrs)
 
 
 class WordForm(forms.ModelForm):
