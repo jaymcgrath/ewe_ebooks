54f7d116012eeca484732026c152b945cd7234f8
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-01-10 16:02:31 -0800

    fix old secrets.py global var ref

--- extras/twittstopher.py
+++ extras/twittstopher.py
@@ -180,8 +180,8 @@ class TwitterUser:
 
     def __init__(self, twitter_username):
         # TODO: Login to twitter for corpus generation using end user's credentials
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+        auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
 
         # Connect to Twitter - raise TweepError if we brick out on this
         try:
