40b644cd7a500d7cfe96daf21369546153558e4b
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-01-12 20:17:41 -0800

    add LoginForm subclassing AuthenticationForm

--- people/forms.py
+++ people/forms.py
@@ -1,7 +1,34 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
 
 from .models import Profile
+
+class LoginForm(AuthenticationForm):
+    """
+    Custom login form for authentication, with a few bootstrap classes attached
+    """
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        # Set some additional field attributes
+
+        username_attrs = {
+                'class': 'form-control',
+                'id': 'lg_username',
+                'placeholder': 'Your Username',
+                'aria-label': 'Input for username',
+                }
+        self.fields['username'].widget.attrs.update(username_attrs)
+
+        password_attrs = {
+                'class':'form-control',
+                'id': 'lg_password',
+                'placeholder': 'Your Password',
+                'aria-label': 'Input for password'
+                }
+        self.fields['password'].widget.attrs.update(password_attrs)
 
 
 class UserForm(forms.ModelForm):
