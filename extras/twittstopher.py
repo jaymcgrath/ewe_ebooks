16bc4a97b338f4af4b7b2878ea01ff6d44e11ffd
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-01-14 18:08:16 -0800

    remove os module calls from inline code, set globals instead

--- extras/twittstopher.py
+++ extras/twittstopher.py
@@ -10,8 +10,13 @@ from nltk import ngrams, word_tokenize, sent_tokenize
 
 
 # from secrets.secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-#  unused in heroku deploy ^
+#  unused in heroku deploy ^, using environment vars instead (see below)
 
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
 class Timeline:
     """
@@ -62,8 +67,8 @@ class Timeline:
 
     def __init__(self, username, count=200, last_tweet_id=1):
         # TODO: Login to twitter for corpus generation using end user's credentials
-        auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-        auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
         # Connect to Twitter - raise
         try:
@@ -180,8 +185,8 @@ class TwitterUser:
 
     def __init__(self, twitter_username):
         # TODO: Login to twitter for corpus generation using end user's credentials
-        auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-        auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+        auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
         # Connect to Twitter - raise TweepError if we brick out on this
         try:
