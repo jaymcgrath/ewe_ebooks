9058cb1dc1bc0e812ca3b47822a48b0c3713ebe3
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-01-28 10:52:54 -0800

    fixes #9 - error thrown by VerifiedEmailRequiredMixin if user is not logged in

--- extras/mixins.py
+++ extras/mixins.py
@@ -5,19 +5,17 @@ from django.shortcuts import render
 
 
 class VerifiedEmailRequiredMixin(AccessMixin):
-    """
+    """ Require user with verified email
     CBV mixin which verifies that the current user is authenticated. If they are not, it resends a verification
     email and returns a page telling them to verify.
     """
     def dispatch(self, request, *args, **kwargs):
-        if not EmailAddress.objects.filter(user=request.user, verified=True).exists():
+        """ Confirm valid user,and then confirm a verified email. """
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        elif not EmailAddress.objects.filter(user=request.user, verified=True).exists():
             send_email_confirmation(request, request.user)
             # Should maybe be some kind of reverse blah blah blah
             return render(request,
                           'account/verified_email_required.html')
         return super().dispatch(request, *args, **kwargs)
-
-
-
-
-
