6b376049df9025045754cf694235b6a82a6e4faf
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-02-27 16:49:37 -0800

    add actual twitter API post to cron

--- bots/cron.py
+++ bots/cron.py
@@ -5,11 +5,20 @@ from django_cron import CronJobBase, Schedule
 from bots.models import Bot, Tweet
 from content.models import Output
 
+import os
+
+import logging
+import tweepy
+
+# Fetch ewe_ebooks twitter API keys
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+
 
 class GenerateTweets(CronJobBase):
     """Scheduler for periodically tweeting for overdue bots"""
 
-    RUN_EVERY_MINS = 5 # every half hour
+    RUN_EVERY_MINS = 1  # every half hour
 
     schedule = Schedule(run_every_mins = RUN_EVERY_MINS)
     code = 'bots.generate_tweets'    # a unique code
@@ -32,3 +41,32 @@ class GenerateTweets(CronJobBase):
 
                 this_tweet = Tweet(bot=this_bot, output=this_output, mashup=this_mashup)
                 this_tweet.save()
+
+                try:
+                    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+                    assert(this_bot.access_token and this_bot.access_token_secret)
+                    auth.set_access_token(this_bot.access_token, this_bot.access_token_secret)
+
+                    api = tweepy.API(auth)
+                    status = api.update_status(status=this_output.body)
+                    this_tweet.tweet_id = status.id
+                    this_tweet.created_twitter = status.created_at
+                    this_tweet.text = status.text
+                    this_tweet.screen_name = status.user.screen_name
+
+                    this_tweet.save()
+
+
+                    # Retreive details of tweet and write to Tweet object
+
+                except (AssertionError, ValueError, tweepy.TweepError) as e:
+                    # Get an instance of a logger
+                    logger = logging.getLogger(__name__)
+                    err_template = 'Error posting from {bot}: {err}'
+                    logger.error(err_template.format(bot=this_bot.name, err=e))
+
+
+
+
+
+
