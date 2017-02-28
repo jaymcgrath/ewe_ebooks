8ae5646feefea93fcda8310ad969db6d7bb90131
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-02-28 08:05:31 -0800

    fix dir for collectstatic

--- ewe_ebooks/settings.py
+++ ewe_ebooks/settings.py
@@ -203,13 +203,15 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    )
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
